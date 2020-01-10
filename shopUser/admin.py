12571960c5cd3e1a1d8f6f4918a7e64b8013e702
fuadashraful <fuadashraful16@gmail.com>
2020-01-10 12:08:43 +0600

    slug field added

--- shopUser/admin.py
+++ shopUser/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import CurrentOffer,Category
+from .models import CurrentOffer,Category,Product
 
 # Register your models here.
-admin.site.register(CurrentOffer)
-admin.site.register(Category)
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(CurrentOffer,AuthorAdmin)
+admin.site.register(Category,AuthorAdmin)
+admin.site.register(Product,AuthorAdmin)
 
 
 admin.site.site_header = "সদাই অনলাইন দোকান"
--- shopUser/models.py
+++ shopUser/models.py
@@ -2,9 +2,11 @@ from django.db import models
 import sys
 from PIL import Image
 from io import BytesIO
+from django.shortcuts import reverse
+from django.utils.text import slugify
 from django.core.files.uploadedfile import InMemoryUploadedFile
-# Create your models here.
-# for image compressing https://github.com/gajeshbhat/django-experiments/blob/master/Upload-compress/Compress/models.py
+from django.db.models.signals import pre_save
+
 
 class CurrentOffer(models.Model):
 
@@ -38,11 +40,8 @@ class CurrentOffer(models.Model):
 class Category(models.Model):
     name=models.CharField(max_length=200)
     categoryImage=models.ImageField(upload_to='categoryImage/',blank=False,null=True)
+    slug=models.SlugField(default='my-category')
     
-    def save(self,*args,**kwargs):
-        if not self.id:
-            self.categoryImage=self.compressImage(self.categoryImage)
-            super(Category,self).save(*args,**kwargs)
 
 
     def compressImage(self,categoryImage):
@@ -54,9 +53,57 @@ class Category(models.Model):
         categoryImage= InMemoryUploadedFile(outputIoStream,'ImageField', "%s.jpg" % categoryImage.name.split('.')[0], 'image/jpeg', sys.getsizeof(outputIoStream), None)
         return categoryImage
     
+ 
+    def save(self,*args,**kwargs):
+        self.categoryImage=self.compressImage(self.categoryImage)
+        super(Category,self).save(*args,**kwargs)
 
     def __str__(self):
         return self.name
-    
-        
 
+    def get_absolute_url(self):
+        return reverse('core:category',kwargs={
+            'slug':self.slug
+        })
+    
+ 
+class Product(models.Model):
+    name=models.CharField(max_length=200)
+    price=models.FloatField()
+    discount_price=models.FloatField(blank=True,null=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    description=models.TextField()
+    image=models.ImageField(upload_to='product/',blank=False)
+    slug = models.SlugField(default='my-product',unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("core:product",kwargs={
+            'slug':self.slug
+        })
+
+    def save(self,*args,**kwargs):
+        print("Save is called")
+        super(Product,self).save(*args,**kwargs)
+
+
+ 
+def unique_slug_generator(instance):
+    constant_slug=slugify(instance.name)
+    slug=constant_slug
+    instanceClass=instance.__class__
+    num=0
+    while instanceClass.objects.filter(slug=slug).exists():
+        num+=1
+        slug=("{slug}-{num}".format(slug=constant_slug, num=num))
+    return slug
+    
+def pre_save_reciever(sender,instance,*args,**kwargs):
+    if not instance.slug:
+        instance.slug=unique_slug_generator(instance)
+
+pre_save.connect(pre_save_reciever,sender=Category)
+pre_save.connect(pre_save_reciever,sender=Product)
+ 
